92967cabcfeece533caf5d807c510a8405e6f2e2
NicolasAndrehh <nicolasolaya22@gmail.com>
2023-06-27 15:26:37 -0500

    Create seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user = User.create(name: "John", email: "john@gmail.com", password: "123456", password_confirmation: "123456")
+
+first_food = Food.create(name: "Apple", user_id: user.id, measurement_unit: "grams", price: 10, quantity: 1)
+second_food = Food.create(name: "Banana", user_id: user.id, measurement_unit: "grams", price: 20, quantity: 1)
+third_food = Food.create(name: "Orange", user_id: user.id, measurement_unit: "grams", price: 30, quantity: 1)
