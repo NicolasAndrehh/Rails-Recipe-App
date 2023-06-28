c9c1433943391c92baa268ba1204b9e42a32e786
NicolasAndrehh <nicolasolaya22@gmail.com>
2023-06-28 16:25:59 -0500

    Create food unit test

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Recipes', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
+      allow_any_instance_of(ApplicationController).to receive(:authorize!).and_return(true)
+      
       get '/recipes/index'
       expect(response).to have_http_status(:success)
     end
