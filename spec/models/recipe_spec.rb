fcebf2909452c3facc00f898d5a61bef9c599b9c
NicolasAndrehh <nicolasolaya22@gmail.com>
2023-06-28 18:33:57 -0500

    Fix Rubocop errors

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  
   before(:each) do
-    @user = User.create(name: "John", email: "john@gmail.com", password: "123456", password_confirmation: "123456")
+    @user = User.create(name: 'John', email: 'john@gmail.com', password: '123456', password_confirmation: '123456')
     @recipe = Recipe.new(name: 'Sandwich', description: 'Is a sandwich', preparation_time: '10 min', cooking_time: '3 min', public: true, user_id: @user.id)
   end
 
--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Recipes', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
       allow_any_instance_of(ApplicationController).to receive(:authorize!).and_return(true)
-      
+
       get '/recipes/index'
       expect(response).to have_http_status(:success)
     end
