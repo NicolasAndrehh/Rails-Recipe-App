e3af367c57443615ee0f28f70c8518471b16573c
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-06-29 22:58:42 +0500

    fix Linters

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,25 +1,26 @@
 class ShoppingListsController < ApplicationController
-    skip_before_action :authenticate_user!, only: %i[index]
-  
-    def index
-      @recipes = Recipe.where(user_id: current_user.id)
-      recipe_ids = @recipes.select(:id)
-      recipe_foods = RecipeFood.where(recipe_id: recipe_ids).group(:food_id).sum(:quantity)
-      @shopping_lists = []
-  
-      recipe_foods.keys.each do |food_id|
-        food = Food.find(food_id)
-        if food.quantity.present? && recipe_foods[food_id].present?
-          needed_food = food.quantity - recipe_foods[food_id]
-          next unless needed_food.negative?  
+  skip_before_action :authenticate_user!, only: %i[index]
+
+  def index
+    @recipes = Recipe.where(user_id: current_user.id)
+    recipe_ids = @recipes.select(:id)
+    recipe_foods = RecipeFood.where(recipe_id: recipe_ids).group(:food_id).sum(:quantity)
+    @shopping_lists = []
+
+    recipe_foods.keys.each do |food_id|
+      food = Food.find(food_id)
+      if food.quantity.present? && recipe_foods[food_id].present?
+        needed_food = food.quantity - recipe_foods[food_id]
+        next unless needed_food.negative?
+
         name = food.name
         needed = needed_food.abs
         price = needed * food.price
         @shopping_lists << { name:, quantity: needed, price: }
       end
-  
+
       @total_price = 0
-      @shopping_lists.each do |food_id|
+      @shopping_lists.each do |_shopping_list_item_id|
         @total_price += food_id[:price]
       end
     end
