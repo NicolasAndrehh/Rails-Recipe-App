9aeaa82c646222d828b17e336113df3574ad3793
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-06-29 22:41:35 +0500

    Fixe Linters

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -2,6 +2,7 @@ class RecipeFoodsController < ApplicationController
   def index
     @recipe_foods = RecipeFood.all
   end
+
   def new
     @recipe = Recipe.find(params[:recipe_id])
     @recipe_food = @recipe.recipe_foods.new
