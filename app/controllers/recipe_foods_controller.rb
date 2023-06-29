eb5e50d01d51f0e120c2faa76a57850058443259
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-06-29 17:32:03 +0500

    shopping list view

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,4 +1,7 @@
 class RecipeFoodsController < ApplicationController
+  def index
+    @recipe_foods = RecipeFood.all
+  end
   def new
     @recipe = Recipe.find(params[:recipe_id])
     @recipe_food = @recipe.recipe_foods.new
